3069fbcae6631f7fc3cc0e634fe0a6ae77353a1f
FKRIK <fernandokrik@hotmail.com>
2022-09-25 21:35:38 -0300

    adicionado teste pontuacao

--- app/src/main/java/up/br/techquizz/QuizzActivity.java
+++ app/src/main/java/up/br/techquizz/QuizzActivity.java
@@ -17,6 +17,9 @@ public class QuizzActivity extends AppCompatActivity {
     Button opt2;
     Button opt3;
     Button opt4;
+    TextView pontua;
+    int pontuacao = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +27,8 @@ public class QuizzActivity extends AppCompatActivity {
 
 
         //busca na instancia de shared preference o conteúdo (nome do jogador) salvo com a chave (playerNameKey)
-        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.mainPrefsKey), Context.MODE_PRIVATE);
-        String playerName = sharedPref.getString(getString(R.string.playerNameKey), "TESTE");
+//        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.mainPrefsKey), Context.MODE_PRIVATE);
+//        String playerName = sharedPref.getString(getString(R.string.playerNameKey), "TESTE");
 
 
     }
@@ -33,25 +36,55 @@ public class QuizzActivity extends AppCompatActivity {
     public void trocarPergunta(View view)
     {
         pergunta = findViewById(R.id.txt_pergunta);
-        opt1 = findViewById(R.id.opt_1);
-        opt2 = findViewById(R.id.opt_2);
-        opt3 = findViewById(R.id.opt_3);
-        opt4 = findViewById(R.id.opt_4);
+        opt1 = findViewById(R.id.btn_opt1);
+        opt2 = findViewById(R.id.btn_opt2);
+        opt3 = findViewById(R.id.btn_opt3);
+        opt4 = findViewById(R.id.btn_opt4);
+        pontua = findViewById(R.id.txt_pontua);
         String nova_pergunta = "";
         String nova_opt1 = "";
         String nova_opt2 = "";
         String nova_opt3 = "";
         String nova_opt4 = "";
 
-        if(pergunta.getText().toString().contains("w")){
+        if(pergunta.getText().toString().contains("website")){
+            if(opt3.isPressed()){
+                pontuacao += 5;
+            }else {
+                pontuacao += 0;
+            }
             nova_pergunta = "Qual o comando git para mudar de branch?";
-            nova_opt1 = "git checkout <nome-da-branch>";
-        } else{
+            nova_opt1 = "git branch --checkout <nome-da-branch>";
+            nova_opt2 = "git checkout -b <nome-da-branch>";
+            nova_opt3 = "git checkout <nome-da-branch>";
+            nova_opt4 = "git branch <nome-da-branch>";
+        } else if(pergunta.getText().toString().contains("branch")){
+            if(opt3.isPressed()){
+                pontuacao += 5;
+            }else {
+                pontuacao += 0;
+            }
             nova_pergunta = "Qual dessas linguagens é fracamente tipada?";
             nova_opt1 = "Ruby";
             nova_opt2 = "C++";
             nova_opt3 = "Scala";
             nova_opt4 = "Dart";
+        } else {
+            if(opt1.isPressed()){
+                pontuacao += 5;
+            }else {
+                pontuacao += 0;
+            }
+            nova_pergunta = "Como centralizar horizontalmente uma div container?";
+            nova_opt1 = ".container {max-width: 1200px; margin: 0 auto}";
+            nova_opt2 = ".container {max-width: 1200px; center: 0 auto}";
+            nova_opt3 = ".container {max-width: 1200px; padding: 0 auto}";
+            nova_opt4 = ".container {max-width: 1200px; center-h: auto}";
+            if(opt1.isPressed()){
+                pontuacao += 5;
+            }else {
+                pontuacao += 0;
+            }
         }
 
         pergunta.setText(nova_pergunta);
@@ -59,6 +92,6 @@ public class QuizzActivity extends AppCompatActivity {
         opt2.setText(nova_opt2);
         opt3.setText(nova_opt3);
         opt4.setText(nova_opt4);
-
+        pontua.setText("Pontuação: " + pontuacao);
     }
 }
